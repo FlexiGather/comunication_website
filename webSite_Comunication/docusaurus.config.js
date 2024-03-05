3884d4065d407b0dc01464c29c488dab373ff650
Roberto Castro <robertorcastro@ua.pt>
2024-03-05 00:35:20 +0000

    add: build

--- webSite_Comunication/docusaurus.config.js
+++ webSite_Comunication/docusaurus.config.js
@@ -15,7 +15,7 @@ const config = {
   url: 'https://comunication_website.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/comunication_website/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
